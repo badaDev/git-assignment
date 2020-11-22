8f856d87a7b19024964d2506cbc2ab3f87bca56e
Tijae007 <tijaniyunus07@gmail.com>
2020-11-22 11:23:36 +0100

    TJ_changes

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 let i = 0;
 function changeColor() {
     let name = document.getElementById("lanre");
-    let colors = ['#8aae4f', '#e17a90', '#311e76', '#8f403d', '#528f3d'];
+    let colors = ['black', 'blue', 'green', 'grey', 'brown'];
     name.style.backgroundColor = colors[i];
     i = (i +1) % colors.length;
     
